cf3445a7b8f55c879141bf82fb8cb2eff110187c
Ashish <54847725+ashish807@users.noreply.github.com>
2020-12-16 18:20:56 +0545

    GitHub Host

---
+++ index.js
@@ -0,0 +1,185 @@
+const auth =firebase.auth();
+
+function checkUserFullName(){
+    var userSurname = document.getElementById("userFullName").value;
+    var flag = false;
+    if(userSurname === ""){
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("usernameError").innerHTML="required&nbspField";
+    }else{
+        document.getElementById("usernameError").innerHTML="";
+    }
+}
+// xxxxxxxxxx Email Validation xxxxxxxxxx
+function checkUserEmail(){
+    var userEmail = document.getElementById("userEmail");
+    var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var flag;
+    if(userEmail.value.match(userEmailFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }
+    if(flag){
+       document.getElementById("userEmailError").innerHTML="Please&nbspcheck&nbspyour&nbspemail&nbspaddress";
+    }else{
+        document.getElementById("userEmailError").style.display="none";
+    }
+}
+// xxxxxxxxxx Password Validation xxxxxxxxxx
+function checkUserPassword(){
+    var userPassword = document.getElementById("userPassword");
+    var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+    var flag;
+    if(userPassword.value.match(userPasswordFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }    
+    if(flag){
+        document.getElementById("userPasswordError").innerHTML="Your&nbsppassword&nbspmust&nbspbe&nbspuppercase,&nbsplowercase&nbspand&nbspmore&nbspthan&nbsp10&nbspcharacters.";
+    }else{
+        document.getElementById("userPasswordError").style.display = "none";
+    }
+}
+// xxxxxxxxxx Submitting and Creating new user in firebase authentication xxxxxxxxxx
+function signUp(){
+    var userFullName = document.getElementById("userFullName").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var userPassword = document.getElementById("userPassword").value;
+    var userFullNameFormate = /^([A-Za-z.\s_-])/;    
+    var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+
+    var checkUserFullNameValid = userFullName.match(userFullNameFormate);
+    var checkUserEmailValid = userEmail.match(userEmailFormate);
+    var checkUserPasswordValid = userPassword.match(userPasswordFormate);
+
+    if(checkUserFullNameValid == null){
+        return checkUserFullName();
+    }else if(checkUserEmailValid == null){
+        return checkUserEmail();
+    }else if(checkUserPasswordValid == null){
+        return checkUserPassword();
+    }else{
+        firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).then((success) => {
+            var user = firebase.auth().currentUser;
+            var uid;
+            if (user != null) {
+                uid = user.uid;
+            }
+            var firebaseRef = firebase.database().ref();
+            var userData = {
+                userFullName: userFullName,
+                userEmail: userEmail,
+                userPassword: userPassword,
+            }
+            firebaseRef.child(uid).set(userData);
+            swal('Your Account Created','Your account was created successfully, you can log in now.',
+            ).then((value) => {
+                setTimeout(function(){
+                    window.location.replace("index.html");
+                }, 1000)
+            });
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            swal({
+                type: 'error',
+                title: 'Error',
+                text: "Email Address already registered",
+            })
+        });
+    }
+}
+function checkUserSIEmail(){
+    var userSIEmail = document.getElementById("userSIEmail");
+    var userSIEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var flag;
+    if(userSIEmail.value.match(userSIEmailFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userSIEmailError").innerHTML = "Check&nbspEmail&nbspAddress";
+    }else{
+        document.getElementById("userSIEmailError").style.display = "none";
+    }
+}
+// xxxxxxxxxx Sign In Password Validation xxxxxxxxxx
+function checkUserSIPassword(){
+    var userSIPassword = document.getElementById("userSIPassword");
+    var userSIPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+    var flag;
+    if(userSIPassword.value.match(userSIPasswordFormate)){
+        flag = false;
+    }else{
+        flag = true;
+    }    
+    if(flag){
+        document.getElementById("userSIPasswordError").innerHTML = "Wrong&nbsppassword&nbspformat";
+    }else{
+        document.getElementById("userSIPasswordError").style.display = "none";
+    }
+}
+// xxxxxxxxxx Check email or password exsist in firebase authentication xxxxxxxxxx    
+function signIn(){
+    var userSIEmail = document.getElementById("userSIEmail").value;
+    var userSIPassword = document.getElementById("userSIPassword").value;
+    var userSIEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var userSIPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+
+    var checkUserEmailValid = userSIEmail.match(userSIEmailFormate);
+    var checkUserPasswordValid = userSIPassword.match(userSIPasswordFormate);
+
+    if(checkUserEmailValid == null){
+        return checkUserSIEmail();
+    }else if(checkUserPasswordValid == null){
+        return checkUserSIPassword();
+    }else{
+        firebase.auth().signInWithEmailAndPassword(userSIEmail, userSIPassword).then((success) => {
+            swal({
+                type: 'successfull',
+                title: 'Succesfully signed in', 
+            }).then((value) => {
+                setTimeout(function(){
+                    window.location.replace("MinorProjectHost/Navigation_and_Homepage/Nav.html");
+                }, 1000)
+            });
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            swal({
+                type: 'error',
+                title: 'Error',
+                text: "Error",
+            })
+        });
+    }
+}
+function signOut(){
+    firebase.auth().signOut().then(function() {
+        //Sign Out successful
+        swal({
+            type: 'successfull',
+            title: 'Signed Out', 
+        }).then((value) => {
+            setTimeout(function(){
+                window.location.replace("../../index.html");
+            }, 1000)
+        });
+    }).catch(function(error) {
+        let errorMessage = error.message;
+        swal({
+            type: 'error',
+            title: 'Error',
+            text: "Error",
+        })
+    });
+}
+
